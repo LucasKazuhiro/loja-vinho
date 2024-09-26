bbf77b1926369f65b5573c1c28e53217fba18d32
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-09-26 17:49:44 -0300

    feat(global): cria componentes e classes

---
+++ src/app/model/item.ts
@@ -0,0 +1,7 @@
+import { Vinho } from "./vinho";
+
+export class Item {
+    public codigo:number = 0;
+    public vinho:Vinho = new Vinho();
+    public quantidade:number = 0;
+}
---
+++ src/app/model/vinho.ts
@@ -0,0 +1,12 @@
+export class Vinho {
+    public codigo:number = 0;
+    public nome:string = "";
+    public tipo:string = "";
+    public uva:string = "";
+    public volume:number = 0;
+    public teorAlcoolico:number = 0;
+    public marca:string = "";
+    public pais:string = "";
+    public regiao:string = "";
+    public preco:number = 0;
+}
