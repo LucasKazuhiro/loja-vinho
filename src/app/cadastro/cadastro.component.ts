f98be44683e7793ed32d2497e6ce12b799b45ac8
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-09-27 01:13:09 -0300

    feat(cadastro): permite o usuario se cadastrar

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  constructor(){
+    this.carregar();
+  }
 
+
+  public cliente:Cliente = new Cliente();
+  public mensagem:String = "";
+  public cadastrarBtnText:String = "";
+
+  public carregar(){
+    let json:any = localStorage.getItem("cliente");
+
+    if(json!=null){
+      this.cliente = JSON.parse(json);
+      this.cadastrarBtnText = "Atualizar dados";
+    }
+    else{
+      this.mensagem = "Insira os dados para um novo cadastro";
+      this.cadastrarBtnText = "Criar uma nova conta";
+    }
+  }
+
+  public salvarCadastro(){
+    localStorage.setItem("clienteMemoria", JSON.stringify(this.cliente));
+    this.mensagem = "Cadastro atualizado com sucesso!";
+    window.location.href="./login";
+  }
 }
