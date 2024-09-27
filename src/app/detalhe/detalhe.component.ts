9776037e803466aef474ca441a7dd8bffec71f7c
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-09-27 16:51:04 -0300

    feat(detalhe): adiciona mais css para estilizar a pagina

--- src/app/detalhe/detalhe.component.ts
+++ src/app/detalhe/detalhe.component.ts
@@ -12,18 +12,29 @@ import { MenuComponent } from '../menu/menu.component';
 })
 export class DetalheComponent {
   public vinho:Vinho = new Vinho();
-  public tags:string = "";
+  public precoComDesconto:number = 0;
+  public quantidadeItemsAtual:number = 1;
 
   constructor(){
     let vinhoJSON:any = localStorage.getItem('vinho');
 
     if(vinhoJSON!=null){
       this.vinho = JSON.parse(vinhoJSON);
-
-      
+      this.precoComDesconto = Number(this.vinho.preco - (this.vinho.preco * this.vinho.desconto));
     }
     else{
       // error ==> produto não encontrado
+    }
+  }
+
+  quantidadeAumentar(){
+    if(this.quantidadeItemsAtual < this.vinho.quantidade){
+      this.quantidadeItemsAtual = this.quantidadeItemsAtual + 1;
+    }
+  }
+  quantidadeDiminuir(){
+    if(this.quantidadeItemsAtual > 1){
+      this.quantidadeItemsAtual = this.quantidadeItemsAtual - 1;
     }
   }
 }
