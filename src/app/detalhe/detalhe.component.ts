62385388847536f1aad69a4ef53698d261c0ebbe
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-09-27 00:07:58 -0300

    feat(detalhe): pega as informações do item clicado e exibe na tela

--- src/app/detalhe/detalhe.component.ts
+++ src/app/detalhe/detalhe.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Vinho } from '../model/vinho';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detalhe',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detalhe.component.html',
   styleUrl: './detalhe.component.css'
 })
 export class DetalheComponent {
+  public vinho:Vinho = new Vinho()
 
+  constructor(){
+    let vinhoJSON:any = localStorage.getItem('vinho')
+
+    if(vinhoJSON!=null){
+      this.vinho = JSON.parse(vinhoJSON)
+    }
+    else{
+      // error ==> produto não encontrado
+    }
+  }
 }
