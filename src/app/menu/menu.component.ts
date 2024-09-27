866eb0ca7806ca2eab8327c599241bde7b11c97e
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-09-27 01:18:02 -0300

    feat(menu): cria o componente do menu

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+  constructor(){
+    this.puxarClienteLogado();
+  }
+
+  public cliente:Cliente = new Cliente();
+  public mensagemBemVindo:string = "";
+  public estaLogado:boolean=false;
+
+  puxarClienteLogado(){
+    let clienteJSON:any = localStorage.getItem("cliente");
+
+    if(clienteJSON!=null){
+      this.cliente = JSON.parse(clienteJSON);
+      this.estaLogado=true;
+    }
+  }
+}
