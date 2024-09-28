83d94f43a0065dee547926210cd6091fb33079fc
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-09-28 20:15:12 -0300

    feat(cesta): cria a estrutura da pagina do carrinho

--- src/app/cesta/cesta.component.ts
+++ src/app/cesta/cesta.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { Item } from '../model/item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cesta',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, CommonModule],
   templateUrl: './cesta.component.html',
   styleUrl: './cesta.component.css'
 })
-export class CestaComponent {
 
+
+export class CestaComponent {
+  public carrinhoDeItems:Item[] = [
+    {codigo: 1, 
+    vinho: {codigo:17, nome:"Miolo Lote 43 ",                                tipo:"Tinto",   uva:"Cabernet Sauvignon, Merlot",       volume:750, teorAlcoolico:14,   marca:"Salentein",                  cdPais:"1",  pais:"Argentina", regiao:"Mendoza",   preco:109.00,  quantidade:10, desconto:0.13},
+    quantidade: 5}
+  ];
+  
+
+  
+  
+  // carrinho.itens = [
+  //   {},
+  //   {codigo:18, nome:"Baron Philippe de Rothschild Escudo Rojo Reserva", tipo:"Tinto",uva:"Cabernet Sauvignon, Carmenere",    volume:750, teorAlcoolico:14,   marca:"Escudo Rojo",                cdPais:"2",  pais:"Chile",     regiao:"Valle Central", preco:95.00,  quantidade:10, desconto:0.17},
+  //   {codigo:19, nome:"Herdade de São Miguel Colheita Selecionada",    tipo:"Tinto",   uva:"Syrah, Touriga Nacional",          volume:750, teorAlcoolico:14,   marca:"Herdade de São Miguel",      cdPais:"7",  pais:"Portugal",  regiao:"Alentejano", preco:87.00,  quantidade:10, desconto:0.42},
+  //   {codigo:20, nome:"Quinta do Crasto Superior ",                    tipo:"Tinto",   uva:"Touriga Nacional",   volume:750, teorAlcoolico:14.5, marca:"Quinta do Crast",      cdPais:"7",    pais:"Portugal",  regiao:"Douro",             preco:215.00, quantidade:10, desconto:0.19}
+  // ]
+
+
+  
+ 
+  
 }
