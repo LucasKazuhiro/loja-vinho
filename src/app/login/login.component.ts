39340618d44986b807c999bb0a1f640d8e9f7013
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-01 16:57:34 -0300

    fix(login): impede se logar sem dados e mostra "sair da conta" somente logado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../model/cliente';
 import { FormsModule } from '@angular/forms';
 import { MenuComponent } from '../menu/menu.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MenuComponent, FormsModule],
+  imports: [MenuComponent, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit{
   constructor(){
     this.puxarClienteMemoria();
   }
 
+  ngOnInit(){
+    this.verificarEstaLogado();
+  }
+
+
   public cliente:Cliente = new Cliente();
   public clienteMemoria:Cliente = new Cliente();
   public mensagem:String = "";
+  public estaLogado:boolean = false;
 
   
   puxarClienteMemoria(){
     let clienteJSON:any = localStorage.getItem("clienteMemoria");
-
     if(clienteJSON!=null){
       this.clienteMemoria = JSON.parse(clienteJSON);
     }
@@ -32,19 +39,30 @@ export class LoginComponent {
     }
   }
 
+
   public fazerLogin(){
-    if(this.cliente.email == this.clienteMemoria.email && 
-       this.cliente.senha == this.clienteMemoria.senha){
-      localStorage.setItem('cliente', JSON.stringify(this.cliente));
-      this.mensagem="logado!!";
-    } 
+    if(this.cliente.email == '' || this.cliente.senha == ''){
+      this.mensagem="Email e Senha são obrigatórios!"
+    }
     else{
-      this.mensagem="Usuario ou senha invalido!!";
+      if(this.cliente.email == this.clienteMemoria.email && 
+         this.cliente.senha == this.clienteMemoria.senha){
+        localStorage.setItem('cliente', JSON.stringify(this.cliente));
+        window.location.href="./vitrine"
+      } 
+      else{
+        this.mensagem="Usuario ou senha inválido!";
+      }
     }
   }
 
   public logout(){
     localStorage.removeItem("cliente");
+    localStorage.removeItem("clienteMemoria")     // Remover quando for impossivel cadastrar usuario vazio
     window.location.href="./login";
+  }
+
+  public verificarEstaLogado(){
+    this.estaLogado = localStorage.getItem('cliente') !== null;
   }
 }
