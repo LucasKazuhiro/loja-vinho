74a5bdb917ece043a309a73dd03cd6a94648b643
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-01 12:04:54 -0300

    refactor(menu): remove codigo não utilizado do menu.component.ts

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,30 +11,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent{
-  constructor(){
-    this.puxarClienteLogado();
-  }
-
-  public cliente:Cliente = new Cliente();
-  public mensagemBemVindo:string = "";
   public estaLogado:boolean=false;
-
-  
-  
-  puxarClienteLogado(){
-    let clienteJSON:any = localStorage.getItem("cliente");
-    console.log(clienteJSON);
-    console.log(this.cliente)
-    if(clienteJSON!=null){
-      this.cliente = JSON.parse(clienteJSON);
-      this.estaLogado=true;
-    }
-    else{
-      this.estaLogado=false;  
-    }
-  }
-
-
 
 
   estaAtivoMenuLateral = false;
