9e9f724bf509a55677724a7b29fc769ae25f0709
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-01 22:37:29 -0300

    feat(esqueciSenha): estiliza a pagina de esqueci senha

--- src/app/esqueci-senha/esqueci-senha.component.ts
+++ src/app/esqueci-senha/esqueci-senha.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-esqueci-senha',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, FormsModule],
   templateUrl: './esqueci-senha.component.html',
   styleUrl: './esqueci-senha.component.css'
 })
 export class EsqueciSenhaComponent {
 
+  public mensagemExecucao:string = "";
+  public emailCliente:string = "";
+
+  enviarCodigo(){
+    if(this.emailCliente !== "" && this.emailCliente !== null){
+      this.mensagemExecucao="Um código foi enviado para seu email!";
+    }
+    else{
+      this.mensagemExecucao="Informe um email válido!"
+    }
+  }
 }
