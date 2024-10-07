a425fa5a23bd6a8981329d0ebb19bdc16798f649
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-06 22:35:47 -0300

    feat(cesta): permite aumentar ou diminuir a quantidade de itens

--- src/app/cesta/cesta.component.ts
+++ src/app/cesta/cesta.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { Item } from '../model/item';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Vinho } from '../model/vinho';
 import { Cesta } from '../model/cesta';
 import { FormsModule } from '@angular/forms';
@@ -18,31 +18,6 @@ import { FormsModule } from '@angular/forms';
 
 
 export class CestaComponent {
-  // public carrinhoDeItems:Item[] = [
-  //   {codigo: 1, 
-  //   vinho: {codigo:17, nome:"Miolo Lote 43 ",                                tipo:"Tinto",   uva:"Cabernet Sauvignon, Merlot",       volume:750, teorAlcoolico:14,   marca:"Salentein",                  cdPais:"1",  pais:"Argentina", regiao:"Mendoza",   preco:109.00,  estoque:10, desconto:0.13},
-  //   quantidade: 5},
-  //   {codigo: 2,
-  //   vinho: {codigo:18, nome:"Baron Philippe de Rothschild Escudo Rojo Reserva", tipo:"Tinto",uva:"Cabernet Sauvignon, Carmenere",    volume:750, teorAlcoolico:14,   marca:"Escudo Rojo",                cdPais:"2",  pais:"Chile",     regiao:"Valle Central", preco:95.00,  estoque:10, desconto:0.17},
-  //   quantidade: 7},
-  //   {codigo: 3,
-  //   vinho: {codigo:19, nome:"Herdade de São Miguel Colheita Selecionada",    tipo:"Tinto",   uva:"Syrah, Touriga Nacional",          volume:750, teorAlcoolico:14,   marca:"Herdade de São Miguel",      cdPais:"7",  pais:"Portugal",  regiao:"Alentejano", preco:87.00,  estoque:10, desconto:0.42},
-  //   quantidade: 3},
-  //   {codigo: 4,
-  //   vinho: {codigo:20, nome:"Quinta do Crasto Superior ",                    tipo:"Tinto",   uva:"Touriga Nacional",   volume:750, teorAlcoolico:14.5, marca:"Quinta do Crast",      cdPais:"7",    pais:"Portugal",  regiao:"Douro",             preco:215.00, estoque:10, desconto:0.19},
-  //   quantidade: 5
-  //   },
-  //   {codigo: 1, 
-  //   vinho: {codigo:17, nome:"Miolo Lote 43 ",                                tipo:"Tinto",   uva:"Cabernet Sauvignon, Merlot",       volume:750, teorAlcoolico:14,   marca:"Salentein",                  cdPais:"1",  pais:"Argentina", regiao:"Mendoza",   preco:109.00,  estoque:10, desconto:0.13},
-  //   quantidade: 5},
-  //   {codigo: 2,
-  //   vinho: {codigo:18, nome:"Baron Philippe de Rothschild Escudo Rojo Reserva", tipo:"Tinto",uva:"Cabernet Sauvignon, Carmenere",    volume:750, teorAlcoolico:14,   marca:"Escudo Rojo",                cdPais:"2",  pais:"Chile",     regiao:"Valle Central", preco:95.00,  estoque:10, desconto:0.17},
-  //   quantidade: 7},
-  //   {codigo: 3,
-  //   vinho: {codigo:19, nome:"Herdade de São Miguel Colheita Selecionada",    tipo:"Tinto",   uva:"Syrah, Touriga Nacional",          volume:750, teorAlcoolico:14,   marca:"Herdade de São Miguel",      cdPais:"7",  pais:"Portugal",  regiao:"Alentejano", preco:87.00,  estoque:10, desconto:0.42},
-  //   quantidade: 3}
-  // ]; 
-  
   public cestaCompra:Cesta = new Cesta();
   public mensagem:string = "";
   public valorTotalFinal:number = 0;
@@ -50,18 +25,27 @@ export class CestaComponent {
   public valorDesconto:number = 0;
 
   constructor(){
-    let cestaJSON= localStorage.getItem("cestaCompra");
+    let cestaJSON = localStorage.getItem("cestaCompra");
 
-    if(cestaJSON != null){
+    this.verificarCestaVazia(cestaJSON);
+  }
+
+
+  public verificarCestaVazia(cestaJSON:string | null){
+    if(cestaJSON !== null){
       this.cestaCompra = JSON.parse(cestaJSON);
-
-      this.valorTotalFinal = this.cestaCompra.total;
+      if(this.cestaCompra.itens.length != 0){
+        this.valorTotalFinal = this.cestaCompra.total;
+        this.mensagem = "";
+      }
+      else{
+        this.mensagem = "Cesta vazia, adicione novos itens!";
+      }
     }
     else{
       this.mensagem = "Cesta vazia, adicione novos itens!";
     }
   }
-
 
   public removerItem(itemRemover: Item){
     this.cestaCompra.itens = this.cestaCompra.itens.filter(item => item != itemRemover);
@@ -74,6 +58,9 @@ export class CestaComponent {
     this.aplicarDesconto();
 
     localStorage.setItem("cestaCompra", JSON.stringify(this.cestaCompra));
+
+    let cestaJSON = localStorage.getItem("cestaCompra");
+    this.verificarCestaVazia(cestaJSON);
   }
 
   public limparCesta(){
@@ -96,4 +83,33 @@ export class CestaComponent {
       this.valorDesconto = 0;
     }
   }
+
+  public quantidadeAumentar(item:Item){
+    if(item.quantidade < item.vinho.estoque){
+      this.atualizarDados(item, 1);
+    }
+  }
+
+  public quantidadeDiminuir(item:Item){
+    if(item.quantidade > 1){
+      this.atualizarDados(item, -1);
+    }
+  }
+
+  public atualizarDados(item:Item, qtd:number){
+    for(let i=0; i<this.cestaCompra.itens.length; i++){
+      if(this.cestaCompra.itens[i].codigo == item.vinho.codigo){
+        this.cestaCompra.itens[i].quantidade = item.quantidade + qtd;
+
+        this.cestaCompra.total = this.cestaCompra.total - this.cestaCompra.itens[i].valorTotal;
+        this.cestaCompra.itens[i].valorTotal = (item.vinho.preco - (item.vinho.preco * item.vinho.desconto)) * (item.quantidade + qtd);
+        this.cestaCompra.total = this.cestaCompra.total + this.cestaCompra.itens[i].valorTotal;        
+        
+        localStorage.setItem("cestaCompra", JSON.stringify(this.cestaCompra));
+        this.aplicarDesconto();
+        break;
+      }
+    }
+  }
+
 }
