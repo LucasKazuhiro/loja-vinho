76bea65f5155edcd6dbf3f168881da87ad3071cb
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-12 10:55:20 -0300

    refactor(all): cria o projeto springboot e reorganiza diretorios

---
+++ lojavinho-springboot/src/main/java/com/fatecipiranga/paoo/lojavinho_springboot/LojavinhoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fatecipiranga.paoo.lojavinho_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavinhoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavinhoSpringbootApplication.class, args);
+	}
+
+}
---
+++ lojavinho-springboot/src/test/java/com/fatecipiranga/paoo/lojavinho_springboot/LojavinhoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecipiranga.paoo.lojavinho_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavinhoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
