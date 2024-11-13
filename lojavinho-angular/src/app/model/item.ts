04d5f9db0e631c995f84f3d5118fd517a14ca0f7
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-13 14:25:09 -0300

    feat(model-angular): adiciona atributos para o backend funcionar

--- lojavinho-angular/src/app/model/item.ts
+++ lojavinho-angular/src/app/model/item.ts
@@ -1,7 +1,9 @@
+import { Cesta } from "./cesta";
 import { Vinho } from "./vinho";
 
 export class Item {
   public codigo: number = 0;
+  public cesta: Cesta = new Cesta();
   public vinho: Vinho = new Vinho();
   public quantidade: number = 0;
   public valorTotal: number = 0;
