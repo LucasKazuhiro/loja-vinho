579ae5508a92a044ab3f3b9522d716733cf2e3c1
Pedro Trovo <trovopedro5@gmail.com>
2024-11-16 01:16:51 -0300

    feat(cliente-repo): cria natives query em cliente repository

--- lojavinho-springboot/src/main/java/com/fatecipiranga/paoo/lojavinho_springboot/repository/ClienteRepository.java
+++ lojavinho-springboot/src/main/java/com/fatecipiranga/paoo/lojavinho_springboot/repository/ClienteRepository.java
@@ -1,10 +1,16 @@
 package com.fatecipiranga.paoo.lojavinho_springboot.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.fatecipiranga.paoo.lojavinho_springboot.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    
+    @Query(value = "select * from cliente where email=?1 and senha=?2", nativeQuery = true)
+    Optional<Cliente> login(String email, String senha);
+
+    @Query(value = "select * from cliente where email=?1", nativeQuery = true)
+    Optional<Cliente> recuperaSenha(String email);
 }
