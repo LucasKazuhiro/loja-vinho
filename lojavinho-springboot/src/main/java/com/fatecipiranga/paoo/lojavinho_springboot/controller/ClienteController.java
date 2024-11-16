d1be618b3392616dacc5df3813578462ca02479f
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-16 18:44:13 -0300

    fix(controllers): permite que fonte da requisição seja qualquer uma

--- lojavinho-springboot/src/main/java/com/fatecipiranga/paoo/lojavinho_springboot/controller/ClienteController.java
+++ lojavinho-springboot/src/main/java/com/fatecipiranga/paoo/lojavinho_springboot/controller/ClienteController.java
@@ -4,17 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fatecipiranga.paoo.lojavinho_springboot.model.Cliente;
 import com.fatecipiranga.paoo.lojavinho_springboot.repository.ClienteRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ClienteController {
 
