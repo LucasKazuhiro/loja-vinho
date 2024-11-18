a817ea4054f26c79c6603c66345747e9a679141d
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-17 22:45:49 -0300

    feat(historico-compra): carrega compras do cliente no componente

--- lojavinho-angular/src/app/historico-compra/historico-compra/historico-compra.component.ts
+++ lojavinho-angular/src/app/historico-compra/historico-compra/historico-compra.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../menu/menu.component';
+import { CestaService } from '../../service/cesta.service';
+import { Cesta } from '../../model/cesta';
 
 @Component({
   selector: 'app-historico-compra',
@@ -9,5 +11,30 @@ import { MenuComponent } from '../../menu/menu.component';
   styleUrl: './historico-compra.component.css'
 })
 export class HistoricoCompraComponent {
+  public comprasCliente: Cesta[] = [];
+  public mensagem:String = "";
+  public idClienteLogado:number = 0;
 
+  constructor(cestaService:CestaService){
+    let clienteJSON = localStorage.getItem('cliente');
+    if(clienteJSON === null){
+      this.mensagem = "Cadastre-se no site para efetuar uma compra";
+    }
+    else{
+      this.idClienteLogado = JSON.parse(clienteJSON).codigo;
+    }
+
+    cestaService.carregarComprasCliente(this.idClienteLogado).subscribe({
+      next:(response) =>{
+        this.comprasCliente = response;
+     
+        if(this.comprasCliente.length === 0){
+          this.mensagem = "Você não possui nenhuma compra efetuada!";
+        }
+      },
+      error:(err) =>{
+        this.mensagem = "Parece que houve um erro! Tente novamente mais tarde.";
+      }
+    })
+  }
 }
