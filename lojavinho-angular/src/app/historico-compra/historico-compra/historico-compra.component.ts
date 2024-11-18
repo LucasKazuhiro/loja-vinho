dc2aa97bf0279db7418c6988c106242614ec7d20
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-17 22:14:48 -0300

    feat(historico-compra): cria o componente da pagina de historico de compra

---
+++ lojavinho-angular/src/app/historico-compra/historico-compra/historico-compra.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../../menu/menu.component';
+
+@Component({
+  selector: 'app-historico-compra',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './historico-compra.component.html',
+  styleUrl: './historico-compra.component.css'
+})
+export class HistoricoCompraComponent {
+
+}
