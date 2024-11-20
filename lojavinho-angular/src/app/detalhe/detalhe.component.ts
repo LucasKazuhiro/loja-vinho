9023cc360e618937bc47373544fb61052fa9557f
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-20 14:22:21 -0300

    docs(detalhe): adiciona comentarios para melhorar a legibilidade do código

--- lojavinho-angular/src/app/detalhe/detalhe.component.ts
+++ lojavinho-angular/src/app/detalhe/detalhe.component.ts
@@ -23,11 +23,15 @@ export class DetalheComponent{
   public quantidadeItemsAtual:number = 1;
 
   constructor(private route:ActivatedRoute, private vinhoService:VinhoService, private cestaService:CestaService){
+    // Extrai o valor no campo "id" da url e converte para Number
     this.vinhoId = Number(this.route.snapshot.paramMap.get('id'));
 
+    // Se inscreve na função "carregar()" do VinhoService
     this.vinhoService.carregar(this.vinhoId).subscribe({
       next: (vinhoJSON : Vinho) => {
+        // Salva o vinho retornado em uma variável local
         this.vinho = vinhoJSON;
+        // Calcula o preço do vinho já com o desconto aplicado
         this.precoComDesconto = Number(this.vinho.preco - (this.vinho.preco * this.vinho.desconto));
       },
       error:(err : HttpErrorResponse) => {
@@ -36,26 +40,34 @@ export class DetalheComponent{
     })
   }
 
+  // Aumenta a quantidade de itens
   quantidadeAumentar(){
     if(this.quantidadeItemsAtual < this.vinho.estoque){
       this.quantidadeItemsAtual = this.quantidadeItemsAtual + 1;
     }
   }
+
+  // Diminui a quantidade de itens
   quantidadeDiminuir(){
     if(this.quantidadeItemsAtual > 1){
       this.quantidadeItemsAtual = this.quantidadeItemsAtual - 1;
     }
   }
 
+  // Adiciona o item à cesta
   public adicionarItem(vinho:Vinho){
     let novoItem:Item = new Item();
 
+    // Cria um novo item contendo "vinho"
     novoItem = this.criarNovoItem(vinho);
+    // Chama a função "adicionarItem()" da CestaService
     this.cestaService.adicionarItem(novoItem);
 
+    // Encaminha o usuário para a página da cesta
     window.location.href="/cesta";
   }
 
+  // Cria um novo item contendo "vinho"
   public criarNovoItem(vinho:Vinho){
     let criarItem = new Item();
 
