c26a5e2803805bcfa1b38108b226926723da3aa1
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-20 15:03:38 -0300

    docs(pesquisa): adiciona comentarios para melhorar a legibilidade do código

--- lojavinho-angular/src/app/pesquisa/pesquisa.component.ts
+++ lojavinho-angular/src/app/pesquisa/pesquisa.component.ts
@@ -14,7 +14,9 @@ import { VinhoService } from '../service/vinho.service';
   styleUrl: './pesquisa.component.css'
 })
 export class PesquisaComponent {
+  // Icones da Senha (FontAwesomeModule)
   faLocationDot = faLocationDot;
+
   public itens: Vinho[] = [];  
   public mensagem: string = "";
   public termo: string = "";
@@ -22,33 +24,42 @@ export class PesquisaComponent {
   constructor(private service : VinhoService){}
 
   ngOnInit(): void {
+    // Puxa o termo pesquisado do localStorage e salva em uma var local
     const termoSalvo = localStorage.getItem("pesquisaValorVinho");
     if (termoSalvo) {
-      this.termo = JSON.parse(termoSalvo); 
+      this.termo = JSON.parse(termoSalvo);
+      // Chama a função "pesquisar()"
       this.pesquisar();
     }
   }
 
+  // Pesquisa o termo inserido
   pesquisar() {
+    // Se inscreve na função "pesquisar()" do VinhoService
     this.service.pesquisar(this.termo).subscribe({
       next: (data: Vinho[]) => {
+        // Salva a array de Vinho retornado em uma var local
         this.itens = data;
         if (this.itens.length === 0) {
+          // Mensagem de Aviso
           this.mensagem = "Nenhum produto encontrado!";
         } else {
+          // Mensagem mostrando a quantidade de vinhos encontrados com aquele termo
           this.mensagem = `${this.itens.length} produto(s) encontrados com a palavra "${this.termo}".`;
         }
       },
       error: () => {
+        // Mensagem de erro
         this.mensagem = "Ocorreu um erro, tente novamente mais tarde.";
       }
     });
   }
-  
 
-
+  // Função para ver mais detalhes de um determinado vinho
   public verMais(vinho:Vinho){
+    // Salva o vinho no localStorage
     localStorage.setItem("vinho", JSON.stringify(vinho));
+    // Encaminha para a pagina de detalhe
     window.location.href="./detalhe";
   }
 }
