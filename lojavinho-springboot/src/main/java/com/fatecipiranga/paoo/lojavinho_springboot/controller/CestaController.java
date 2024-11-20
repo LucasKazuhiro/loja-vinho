27550af0692fa077c43dfcaea58420e5434e0f77
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-11-20 11:08:12 -0300

    feat(cesta/cliente): move a função de salvar carrinho para clientecontroller

--- lojavinho-springboot/src/main/java/com/fatecipiranga/paoo/lojavinho_springboot/controller/CestaController.java
+++ lojavinho-springboot/src/main/java/com/fatecipiranga/paoo/lojavinho_springboot/controller/CestaController.java
@@ -1,10 +1,8 @@
 package com.fatecipiranga.paoo.lojavinho_springboot.controller;
 
 import com.fatecipiranga.paoo.lojavinho_springboot.model.Cesta;
-import com.fatecipiranga.paoo.lojavinho_springboot.model.Item;
 import com.fatecipiranga.paoo.lojavinho_springboot.repository.CestaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,30 +13,7 @@ import java.util.List;
 @RestController
 public class CestaController {
     @Autowired
-    CestaRepository bd_cesta;
-
-    @PostMapping("/api/cesta")
-    public ResponseEntity<?> criarCesta(@RequestBody Cesta cesta){
-        try{
-            if(cesta == null){ return ResponseEntity.badRequest().body("Cesta não pode ser nula."); }
-            if(cesta.getCliente() == null) { return ResponseEntity.badRequest().body("O cliente é obrigatório e não pode ser nulo."); }
-            if(cesta.getItens().isEmpty()){ return ResponseEntity.badRequest().body("É necessário pelo menos um produto para efetuar uma compra"); }
-
-            for(Item item : cesta.getItens()){
-                item.setCesta(cesta);
-            }
-
-            if (cesta.getData() == null) {
-                cesta.setData(LocalDate.now());
-            }
-
-            return ResponseEntity.ok(bd_cesta.save(cesta));
-        }
-        catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno no servidor: " + e.getMessage());
-        }
-
-    }
+    private CestaRepository bd_cesta;
 
     @GetMapping("/api/cesta/cliente")
     public ResponseEntity<List<Cesta>> listarCestaCliente(@RequestParam Long clienteCod){
